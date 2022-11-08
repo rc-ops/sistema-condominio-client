83b9cbe820a4596b6c8377679843359d2353db06
rc-ops <rodrigocarvalhocontato@protonmail.com>
2022-11-08 12:42:15 -0300

    Add Morador funcionando

--- src/main/java/api/MoradorApi.java
+++ src/main/java/api/MoradorApi.java
@@ -8,4 +8,6 @@ import retrofit2.http.GET;
 public interface MoradorApi {
     @GET("/moradores")
     Call<List<Morador>> findAll();
+    @POST("/moradores/novo")
+    Call<Void> createMorador(@Body Morador morador);
 }
